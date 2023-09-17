a37aeecadc83882ee9b078b03c5e3e21b4f915ba
Luis181996 <luishumberto181996@hotmail.com>
2023-09-17 17:01:25 -0500

    desarrollo del ejercicio1

--- src/main/java/pe/edu/cibertec/appwebformularios2/model/PromedioModel.java
+++ src/main/java/pe/edu/cibertec/appwebformularios2/model/PromedioModel.java
@@ -1,4 +1,16 @@
 package pe.edu.cibertec.appwebformularios2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class PromedioModel {
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer profin;
 }
